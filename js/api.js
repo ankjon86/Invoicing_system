2dff1cb4062a99f236e27a8c4d10a6f20a288c80
ankjon86 <ankjon86@gmail.com>
2025-12-13 11:02:58 +0000

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,48 +2,43 @@
 class ApiService {
   constructor() {
     // UPDATE THIS with your Google Apps Script Web App URL
-this.BASE_URL = 'https://script.google.com/macros/s/AKfycbxFMMpImLf5BdTkOihOd4RZ-Kk70smJxse8M7sHFrTElgGKXheyOPyIyY0prvPPgVD8/exec';
+    this.BASE_URL = 'https://script.google.com/macros/s/AKfycbxFMMpImLf5BdTkOihOd4RZ-Kk70smJxse8M7sHFrTElgGKXheyOPyIyY0prvPPgVD8/exec';
     this.cache = new Map();
   }
 
-  // Generic request method
+  // Generic request method (JSONP)
   async request(action, data = {}) {
     try {
-      // Show loading indicator
       if (window.Utils) window.Utils.showLoading(true);
 
-      // Use JSONP for cross-domain requests
       return new Promise((resolve, reject) => {
         const callbackName = 'callback_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
         
-        // Create script element
         const script = document.createElement('script');
         const url = new URL(this.BASE_URL);
         url.searchParams.append('action', action);
         url.searchParams.append('data', JSON.stringify(data));
         url.searchParams.append('callback', callbackName);
         
-        // Define callback function
         window[callbackName] = (response) => {
           // Cleanup
-          document.head.removeChild(script);
+          if (script.parentNode) document.head.removeChild(script);
           delete window[callbackName];
           
           if (window.Utils) window.Utils.showLoading(false);
           
-          if (response.success) {
-            // Cache responses
+          if (response && response.success) {
             const cacheKey = `${action}_${JSON.stringify(data)}`;
             this.cache.set(cacheKey, response);
             resolve(response);
           } else {
-            reject(new Error(response.error || 'API request failed'));
+            reject(new Error((response && response.error) || 'API request failed'));
           }
         };
         
         script.src = url.toString();
         script.onerror = () => {
-          document.head.removeChild(script);
+          if (script.parentNode) document.head.removeChild(script);
           delete window[callbackName];
           if (window.Utils) window.Utils.showLoading(false);
           reject(new Error('Network error'));
@@ -102,6 +97,10 @@ this.BASE_URL = 'https://script.google.com/macros/s/AKfycbxFMMpImLf5BdTkOihOd4RZ
     return this.request('get_invoice', { id: invoiceId });
   }
 
+  async updateInvoiceStatus(data) {
+    return this.request('update_invoice_status', data);
+  }
+
   async sendInvoice(invoiceId) {
     return this.request('send_invoice', { invoice_id: invoiceId });
   }
@@ -123,8 +122,29 @@ this.BASE_URL = 'https://script.google.com/macros/s/AKfycbxFMMpImLf5BdTkOihOd4RZ
     return this.request('get_billing_schedules', filters);
   }
 
+  async updateBillingSchedule(data) {
+    return this.request('update_billing_schedule', data);
+  }
+
+  async pauseBillingSchedule(scheduleId) {
+    return this.request('pause_billing_schedule', { id: scheduleId });
+  }
+
+  async resumeBillingSchedule(scheduleId) {
+    return this.request('resume_billing_schedule', { id: scheduleId });
+  }
+
   async getUpcomingInvoices(days = 30) {
     return this.request('get_upcoming_invoices', { days });
+  }
+
+  // Product APIs
+  async addProduct(productData) {
+    return this.request('add_product', productData);
+  }
+
+  async getProducts() {
+    return this.request('get_products', {});
   }
 
   // System APIs
@@ -134,6 +154,23 @@ this.BASE_URL = 'https://script.google.com/macros/s/AKfycbxFMMpImLf5BdTkOihOd4RZ
 
   async getDashboardStats() {
     return this.request('get_stats', {});
+  }
+
+  // Receipts APIs (new)
+  async addReceipt(receiptData) {
+    return this.request('add_receipt', receiptData);
+  }
+
+  async getReceipts(filters = {}) {
+    return this.request('get_receipts', filters);
+  }
+
+  async getReceipt(receiptId) {
+    return this.request('get_receipt', { id: receiptId });
+  }
+
+  async deleteReceipt(receiptId) {
+    return this.request('delete_receipt', { id: receiptId });
   }
 
   // Test connection
